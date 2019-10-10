7422612a90f553b4de63b7b9f43e494670cc8e0d
Justino Mora <jmora258@gmail.com>
2019-10-10 10:52:14 -0700

    version 1 complete.

---
+++ contentScript.js
@@ -0,0 +1,27 @@
+let elements = document.getElementsByTagName('*');
+let totalChanges = 0;
+
+for (let i = 0; i < elements.length; i++) {
+  let element = elements[i];
+  
+  // iterate through child nodes
+  for(let j = 0; j < element.childNodes.length; j++) {
+    let node = element.childNodes[j];
+
+    if (node.nodeType === 3) {
+      let text = node.nodeValue;
+      let changedText = text.replace(/illegal alien|illegal aliens|illegals|illegal immigrant|illegal immigrants/gi, '<span style="background-color: #FFFF00;">undocumented immigrant</span>');
+      
+      if (changedText !== text) {
+	let newNode = document.createElement("span");
+	newNode.innerHTML = changedText;
+	element.replaceChild(newNode, node);
+	totalChanges++;
+      }
+    }
+  }
+}
+const grade = totalChanges > 0 ? 'F' : 'A';
+let message = `No Human Being is Illegal\n\nTotal changes: ${totalChanges}\n This webpage has a grade of: ${grade}`;
+alert(message);
+
