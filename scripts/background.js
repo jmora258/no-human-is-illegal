622a0ed03ca12d5f402b318249304f6229a929f6
Justino Mora <jmora258@gmail.com>
2019-10-18 00:38:50 -0700

    Complete Popup: shows results and disclaimer. Communication between active tab and popup works smoothly. Add styles to elements.

--- scripts/background.js
+++ scripts/background.js
@@ -1,24 +1,22 @@
-let pageMessage = "";
+// 1. Establish connection with ACTIVE TAB 
+// 2. Send request to CONTENT.js and execute code
+// 3. Receive callback with data 
+// 4. Display it on the Popup 
 
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('results').textContent = "Extension loaded";
 
   const button = document.getElementById("getResults");
   button.addEventListener('click', function () {
-    console.log("test storage is: " + pageMessage);
-    $("#results").html(pageMessage);
-    console.log("inside button listener")
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {action: "AbolishICE"}, function (response) {
+        let message = `<p id="primary-message">Total changes made: <strong>${response.data[0]}</strong><br/>This page's grade is: <strong>${response.data[1]}</strong></p> <br/><p id="note">NOTE #1: Navigate to another page OR refresh the page to reset.<br/><br/>NOTE #2: If this page has any dynamic elements and/or social media plugins (Twitter or Facebook feeds), there is a chance that not all anti-immigrant/xenophobic terms were replaced.</p>`;
+
+        $("#results").html(message);
+
+        // no longer need button, remove it 
+        $("#getResults").detach();
+      });
+    });
   });
-});
-
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    pageMessage = request.msg;
-    if (pageMessage) {
-      console.log("message received..." + pageMessage);
-    }
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,35 +1,36 @@
-let elements = document.getElementsByTagName('*');
-let totalChanges = 0;
+// 1. Listen for request from Extension 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    
+  // 2. Process request 
+    if (request.action == "AbolishICE") {
+      console.log("good! execute command received");
 
-for (let i = 0; i < elements.length; i++) {
-  let element = elements[i];
-  
-  // iterate through child nodes
-  for(let j = 0; j < element.childNodes.length; j++) {
-    let node = element.childNodes[j];
+      let elements = document.getElementsByTagName('*');
+      let totalChanges = 0;
 
-    if (node.nodeType === 3) {
-      let text = node.nodeValue;
-      let changedText = text.replace(/illegal alien|illegal aliens|illegals|illegal immigrant|illegal immigrants/gi, '<span class="highlight-me" style="">undocumented immigrant</span>');
-      
-      if (changedText !== text) {
-	let newNode = document.createElement("span");
-	newNode.innerHTML = changedText;
-	element.replaceChild(newNode, node);
-	totalChanges++;
+      for (let i = 0; i < elements.length; i++) {
+        let element = elements[i];
+
+        // 3. Iterate through child nodes and implement changes
+        for(let j = 0; j < element.childNodes.length; j++) {
+          let node = element.childNodes[j];
+
+          if (node.nodeType === 3) {
+            let text = node.nodeValue;
+            let changedText = text.replace(/illegal alien|illegal aliens|illegals|illegal immigrant|illegal immigrants/gi, '<span class="highlight-me" style="">undocumented immigrant</span>');
+            
+            if (changedText !== text) {
+              let newNode = document.createElement("span");
+              newNode.innerHTML = changedText;
+              element.replaceChild(newNode, node);
+              totalChanges++;
+            }
+          }
+        }
       }
-    }
-  }
-}
-const grade = totalChanges > 0 ? 'F' : 'A';
-let message = `No Human Being is Illegal\n\nTotal changes to be made: ${totalChanges}\n This webpage has a grade of: ${grade}`;
+      const grade = totalChanges > 0 ? 'F-' : 'A';
 
-if (typeof(Storage) !== "undefined") {
-    // store data
-    sessionStorage.setItem("popupMessage", message);
-    console.log("Inside storage: " + sessionStorage.getItem("popupMessage"));
-    chrome.runtime.sendMessage({msg : sessionStorage.getItem("popupMessage")});
-} else {
-    // if Storage is not supported or it fails to use storage
-    alert(message);
-}
+      // 4. Send data back to extension
+      sendResponse({data: [totalChanges, grade]});
+    }
+});
