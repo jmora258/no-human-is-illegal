f8f17597c0d980b07d9476fb67cd96cebe8ccaf1
Justino Mora <jmora258@gmail.com>
2019-10-18 15:44:07 -0700

    Fix event listeners for button.

--- scripts/background.js
+++ scripts/background.js
@@ -26,11 +26,10 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('results').textContent = "Extension loaded";
 
   /* event handlers for the popup's 'Get Results' button */
-  const resultsButton = document.querySelectorAll('button');
+  const resultsButton = document.getElementById("getResults");
   resultsButton.addEventListener('click', trackButtonClick);
 
-  const button = document.getElementById("getResults");
-  button.addEventListener('click', function () {
+  resultsButton.addEventListener('click', function () {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {action: "AbolishICE"}, function (response) {
         let message = `<p id="primary-message">Total changes made: <strong>${response.data[0]}</strong><br/>This page's grade is: <strong>${response.data[1]}</strong></p> <br/><p id="note">NOTE #1: Navigate to another page OR refresh the page to reset.<br/><br/>NOTE #2: If this page has any dynamic elements and/or social media plugins (Twitter or Facebook feeds), there is a chance that not all anti-immigrant/xenophobic terms were replaced.</p>`;
